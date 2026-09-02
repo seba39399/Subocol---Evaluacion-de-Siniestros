d58e91b6d4d9e598ae72d2aa6251fb6d1401328a
Sebastian <fluis8270@gmail.com>
2026-09-02 18:50:06 -0500

    fix 6: actualizar politica de iam a 2012-10-17

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,7 +94,7 @@ resource "aws_iam_role" "ecs_execution_role" {
   name = "subocol-ecs-execution-role-${var.environment}"
 
   assume_role_policy = jsonencode({
-    Version = "2010-09-09"
+    Version = "2012-10-17"
     Statement = [{
       Action = "sts:AssumeRole"
       Effect = "Allow"
@@ -116,7 +116,7 @@ resource "aws_iam_role_policy" "s3_access" {
   role = aws_iam_role.ecs_execution_role.id
 
   policy = jsonencode({
-    Version = "2010-09-09"
+    Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
       Action = [
